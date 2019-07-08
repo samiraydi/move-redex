3cf4df8a6e194aebe8e12ec9293a30182171ec78
samiraydi <samiraydi26@gmail.com>
2019-07-08 16:20:06 +0100

    Updates

---
+++ precache-manifest.c43632691e2c31acbde0945b7202ea52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647886fc07c67ed871043c8addfe50a9",
+    "url": "/move-redex/index.html"
+  },
+  {
+    "revision": "6fa6bffe67fd5bb800ac",
+    "url": "/move-redex/static/css/main.28a5b0af.chunk.css"
+  },
+  {
+    "revision": "43beb4689dff68018535",
+    "url": "/move-redex/static/js/2.e7dbb282.chunk.js"
+  },
+  {
+    "revision": "6fa6bffe67fd5bb800ac",
+    "url": "/move-redex/static/js/main.42870fcb.chunk.js"
+  },
+  {
+    "revision": "3ea1615213280559f24a",
+    "url": "/move-redex/static/js/runtime~main.47ffbe23.js"
+  }
+]);
